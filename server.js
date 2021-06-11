61a3563b5978c7ec69b928de230be973019e3456
Danny Sollomoni <dsollomoni@gmail.com>
2021-06-10 22:48:58 -0400

    fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.DB_URL, {
 });
 
 // routes
-app.use(require("/routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
