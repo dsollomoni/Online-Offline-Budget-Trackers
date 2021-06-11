42c550b16084b65401d8c6d6cf85aef578140a28
Danny Sollomoni <dsollomoni@gmail.com>
2021-06-10 23:19:00 -0400

    fixed heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(process.env.DB_URL || "mongodb://localhost/budget",  {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
